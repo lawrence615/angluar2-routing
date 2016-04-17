4a454d942da1b59b5a18dd19a92be161a9cd102d
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-17 20:55:47 +0300

    Adding params to links and routes

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -9,7 +9,7 @@ import {Component2Component} from "./component2.component";
     template: `
         <header>
             <ul>
-                <li><a [routerLink]="['Component1']">Component 1</a></li>
+                <li><a [routerLink]="['Component1', {source:'AppComponent', optional:'This is optional'}]">Component 1</a></li>
                 <li><a [routerLink]="['Component2']">Component 2</a></li>
             </ul>
         </header>
@@ -18,7 +18,7 @@ import {Component2Component} from "./component2.component";
     directives:[ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-    {path: '/component-1', name: 'Component1', component: Component1Component, useAsDefault: true },
+    {path: '/component-1/:source', name: 'Component1', component: Component1Component },
     {path: '/component-2', name: 'Component2', component: Component2Component }
 ])
 export class AppComponent {
