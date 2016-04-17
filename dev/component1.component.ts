3fb3a48206205e9e1ecc85ecefd8958d7d15f5de
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-17 20:11:14 +0300

    initial commit
    These are our two components that we are showing through routing

---
+++ dev/component1.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "angular2/core";
+/**
+ * Created by Lawrence on 4/17/16.
+ */
+
+@Component({
+    template: 'Component 1'
+})
+export class Component1Component {
+
+}
---
+++ dev/component2.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "angular2/core";
+/**
+ * Created by Lawrence on 4/17/16.
+ */
+@Component({
+    template: 'Component 2'
+})
+export class Component2Component {
+
+}
