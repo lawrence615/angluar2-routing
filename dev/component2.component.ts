4483daf60bd059f3d101b7f14fc469bab12e4754
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-17 20:24:44 +0300

    Here we have imperative routing; which means we are triggering a routing action from the code.
    An application to this is for example when a calculation is finished

--- dev/component2.component.ts
+++ dev/component2.component.ts
@@ -1,10 +1,21 @@
 import {Component} from "angular2/core";
+import {Router} from "angular2/router";
 /**
  * Created by Lawrence on 4/17/16.
  */
 @Component({
-    template: 'Component 2'
+    template: `
+        <h2>This is Component 2</h2>
+        <button (click)="onNavigate()">Take me to Component 1</button>
+    `
 })
 export class Component2Component {
 
+    constructor(private _router:Router) {
+
+    }
+
+    onNavigate() {
+        this._router.navigate(['Component1']);
+    }
 }
