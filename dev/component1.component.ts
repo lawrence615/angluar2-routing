0438122961437c3862aff8df2ab7a3f021635da4
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-17 22:36:41 +0300

    We now have soubrettes

--- dev/component1.component.ts
+++ dev/component1.component.ts
@@ -1,6 +1,10 @@
 import {Component} from "angular2/core";
 import {OnInit} from "angular2/core";
 import {RouteParams} from "angular2/router";
+import {RouteConfig} from "angular2/router";
+import {Comp1MainComponent} from "./comp1-main.component";
+import {Comp1SubComponent} from "./comp2-sub.component";
+import {ROUTER_DIRECTIVES} from "angular2/router";
 /**
  * Created by Lawrence on 4/17/16.
  */
@@ -15,13 +19,25 @@ import {RouteParams} from "angular2/router";
         <div>
             Optional was {{ optional }}
         </div>
-    `
+        <div>
+            <a [routerLink]="['Component1Main']">Main</a>
+            <a [routerLink]="['Component1Sub']">Sub</a>
+        </div>
+        <router-outlet></router-outlet>
+    `,
+    directives:[ROUTER_DIRECTIVES]
 })
-export class Component1Component implements OnInit{
-    source: string;
-    optional: string;
 
-    constructor(private _routeParams: RouteParams){}
+@RouteConfig([
+    {path: '/', name: 'Component1Main', component: Comp1MainComponent, useAsDefault: true},
+    {path: '/subroute', name: 'Component1Sub', component: Comp1SubComponent}
+])
+export class Component1Component implements OnInit {
+    source:string;
+    optional:string;
+
+    constructor(private _routeParams:RouteParams) {
+    }
 
     ngOnInit():any {
         this.source = this._routeParams.get('source');
