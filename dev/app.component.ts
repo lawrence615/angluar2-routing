ce5585148df5471ebaf72db186d981231755633c
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-17 20:10:18 +0300

    We removed what we had initially in the template.
    Replaced that with header that has two links; Component 1 and Component
    2.
    
    The routerLink is a property binding directive that takes in the name
    of the routes we have defined in our RouterConfig decorator below.
    
    We also have the router-outlet tags where the routed views go.
    
    Our RouterConfig decorator defines; path:this is the route that will
    appear on the browser, name:this is the name that we use in referring
    to the route and it is the one we are using in the routerLink,
    component:which is the component we want loaded and useAsDefault in the
    first route to show that we want this particular route to be loaded the
    first time

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,26 @@
 import {Component} from 'angular2/core';
+import {RouteConfig} from "angular2/router";
+import {Component1Component} from "./component1.component";
+import {ROUTER_DIRECTIVES} from "angular2/router";
+import {Component2Component} from "./component2.component";
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <header>
+            <ul>
+                <li><a [routerLink]="['Component1']">Component 1</a></li>
+                <li><a [routerLink]="['Component2']">Component 2</a></li>
+            </ul>
+        </header>
+        <router-outlet></router-outlet>
     `,
+    directives:[ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+    {path: '/component-1', name: 'Component1', component: Component1Component, useAsDefault: true },
+    {path: '/component-2', name: 'Component2', component: Component2Component }
+])
 export class AppComponent {
 
 }
