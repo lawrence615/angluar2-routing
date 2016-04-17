692eaa03cb42d5fd1b5ed5ad4cea045f917445d3
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-17 20:53:35 +0300

    We added params to our routerLink. ’source’ is the name by which we identify the parameter we are passing

--- dev/component1.component.ts
+++ dev/component1.component.ts
@@ -1,11 +1,30 @@
 import {Component} from "angular2/core";
+import {OnInit} from "angular2/core";
+import {RouteParams} from "angular2/router";
 /**
  * Created by Lawrence on 4/17/16.
  */
 
 @Component({
-    template: 'Component 1'
-})
-export class Component1Component {
+    template: `
+        This is Component 1
+        <div>
+            Source was {{ source }}
+        </div>
 
+        <div>
+            Optional was {{ optional }}
+        </div>
+    `
+})
+export class Component1Component implements OnInit{
+    source: string;
+    optional: string;
+
+    constructor(private _routeParams: RouteParams){}
+
+    ngOnInit():any {
+        this.source = this._routeParams.get('source');
+        this.optional = this._routeParams.get('optional');
+    }
 }
