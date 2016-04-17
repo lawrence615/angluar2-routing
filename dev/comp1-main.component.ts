99031c18f76455651f805e449dbe00ba0761c263
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-17 22:35:33 +0300

    initial commit
    These are the two soubrettes of the component-1 route.
    Comp1MainComponent is loaded by default when we hit ‘component-1’ route

---
+++ dev/comp1-main.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "angular2/core";
+/**
+ * Created by Lawrence on 4/17/16.
+ */
+
+@Component({
+    selector: 'comp1-main',
+    template: `
+        This is Component 1 Main
+        `
+})
+export class Comp1MainComponent {
+
+}
---
+++ dev/comp2-sub.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "angular2/core";
+/**
+ * Created by Lawrence on 4/17/16.
+ */
+
+@Component({
+    selector: 'comp2-sub',
+    template: `
+        This is Component 2 Sub
+        `
+})
+export class Comp1SubComponent {
+
+}
