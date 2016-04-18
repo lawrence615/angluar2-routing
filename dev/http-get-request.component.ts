18b8d6059e4cff8ec0bdf86422e2f2e4ec693451
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-18 22:15:29 +0300

    initial commit
    In this component we are getting and posting data through a service we
    built

---
+++ dev/http-get-request.component.ts
@@ -0,0 +1,50 @@
+import {Component} from "angular2/core";
+import {HttpService} from "./services/http.service";
+import {error} from "util";
+/**
+ * Created by Lawrence on 4/18/16.
+ */
+
+@Component({
+    selector: 'my-http-get-request',
+    template: `
+        <div class="input">
+            <label for="title">Title</label>
+            <input type="text" id="title" #title>
+        </div>
+        <div class="input">
+            <label for="body">Body</label>
+            <input type="text" id="body" #body>
+        </div>
+        <div class="input">
+            <label for="user-id">User ID</label>
+            <input type="text" id="user-id" #userId>
+        </div>
+        <button (click)="onPost(title.value, body.value, userId.value)">Post Data</button>
+        <button (click)="onGetPosts()">Get All Posts</button>
+        <p>Response: {{response | json}}</p>
+    `,
+    providers: [HttpService]
+})
+export class HttpGetRequestComponent {
+    response:string;
+
+    constructor(private _httpService:HttpService) {
+    }
+
+    onGetPosts() {
+        this._httpService.getPosts()
+            .subscribe(
+                response => this.response = response,
+                error => console.log(error)
+            )
+    }
+
+    onPost(title:string, body:string, userId:string) {
+        this._httpService.createPost({title: title, body: body, userId: +userId})
+            .subscribe(
+                response => this.response = response,
+                error => console.log(error)
+            )
+    }
+}
